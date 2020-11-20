0c4e39a4ca1ee7889b67ab954d00d3e5cd8f9145
Javi123lo <javierferreyra2017@gmail.com>
2020-11-20 18:50:10 -0300

    crenado rama

--- index.php
+++ index.php
@@ -34,8 +34,8 @@
   <!-- Navigation -->
   <nav class="navbar navbar-light bg-light static-top">
     <div class="container">
-      <a class="navbar-brand" id="fuente" href="#">Arcovid</a>
-      <a class="btn btn-primary" href="#">Iniciar sesión</a>
+      <a class="navbar-brand" id="fuente" href="#">Arcovid-19</a>
+      <a class="btn btn-primary" href="#">Iniciar Sesión</a>
     </div>
   </nav>
 
@@ -45,7 +45,7 @@
     <div class="container">
       <div class="row">
         <div class="col-xl-9 mx-auto">
-          <h1 class="mb-5">Build a landing page for your business or project and generate more leads!</h1>
+          <h1 class="mb-5">Un mecanismo para la desinfección del cuerpo, 100% reciclable y echo por alumnos de la Escuela Tecnica N°10!</h1>
         </div>
         <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
           <form>
@@ -54,7 +54,7 @@
                 <input type="email" class="form-control form-control-lg" placeholder="Ingresá tu email...">
               </div>
               <div class="col-12 col-md-3">
-                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
+                <button type="submit" class="btn btn-block btn-lg btn-primary">Enviar</button>
               </div>
             </div>
           </form>
@@ -70,19 +70,19 @@
         <div class="col-lg-4">
           <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
             <div class="features-icons-icon d-flex">
-              <i class="icon-screen-desktop m-auto text-primary"></i>
+              <i class="fas fa-recycle m-auto text-primary"fa-5x></i>
             </div>
-            <h3>Fully Responsive</h3>
-            <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
+            <h3>Muy Rentable</h3>
+            <p class="lead mb-0">Al ser reciclable es mas varato y tiene muy poco natenimiento!</p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
             <div class="features-icons-icon d-flex">
-              <i class="icon-layers m-auto text-primary"></i>
+            <i class="fas fa-thermometer-three-quarters  m-auto text-primary"></i>
             </div>
-            <h3>Bootstrap 4 Ready</h3>
-            <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>
+            <h3>Tiene doble uso</h3>
+            <p class="lead mb-0">Ademas de decinfectar toma la temperatura!</p>
           </div>
         </div>
         <div class="col-lg-4">
@@ -90,8 +90,8 @@
             <div class="features-icons-icon d-flex">
               <i class="icon-check m-auto text-primary"></i>
             </div>
-            <h3>Easy to Use</h3>
-            <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+            <h3>Facil en usar</h3>
+            <p class="lead mb-0">Es muy facil en manipular!</p>
           </div>
         </div>
       </div>
@@ -103,23 +103,23 @@
     <div class="container-fluid p-0">
       <div class="row no-gutters">
 
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-1.jpg');"></div>
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/mascara.jpg');"></div>
         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <h2>Fully Responsive Design</h2>
-          <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+          <h2>Para la vuelta a lo normal </h2>
+          <p class="lead mb-0">Todo esto para el cambio post pandemia para el mejor cuidados en espacios cerrados donde hay una fluides de gente y para las nuevas normas de sanidad, mas los protocolos nuevos que vengan!</p>
         </div>
       </div>
       <div class="row no-gutters">
-        <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
+        <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/mapa.png');"></div>
         <div class="col-lg-6 my-auto showcase-text">
-          <h2>Updated For Bootstrap 4</h2>
-          <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
+          <h2>Lograr la mejora de decteccion</h2>
+          <p class="lead mb-0">Se va lograr que la gente no tome contacto con otros y si llega a tener fiebre, lograr poner el protocolo en accion!</p>
         </div>
       </div>
       <div class="row no-gutters">
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/sala.jpg');"></div>
         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <h2>Easy to Use &amp; Customize</h2>
+          <h2>Pero lo ecencial son las aulas</h2>
           <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
         </div>
       </div>
